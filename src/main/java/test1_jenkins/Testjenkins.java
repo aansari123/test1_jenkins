4a5614ba420b29263854a117ea90e46e83745ea0
aansari123 <aansari@pizzapizza.ca>
2017-04-18 13:11:32 -0400

    Jenkins test project
    Jenkins test project

---
+++ src/main/java/test1_jenkins/Testjenkins.java
@@ -0,0 +1,11 @@
+package test1_jenkins;
+
+public class Testjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Running Test Jenkins.....");
+	}
+
+}
